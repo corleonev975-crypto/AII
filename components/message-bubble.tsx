355aee04e33d2471b6974f809b3639e0b0f454c2
corleonev975-crypto <corleonev975@gmail.com>
2026-04-23 19:06:30 +0700

    Update message-bubble.tsx

--- components/message-bubble.tsx
+++ components/message-bubble.tsx
@@ -11,7 +11,7 @@ export default function MessageBubble({ role, content, image }: Props) {
     <div className={`flex ${isUser ? "justify-end" : "justify-start"}`}>
       <div className="max-w-[80%] rounded-xl bg-white/10 p-3">
         {image && (
-          <img src={image} className="mb-2 rounded-lg max-h-60" />
+          <img src={image} className="mb-2 max-h-60 rounded-lg" />
         )}
         <p>{content}</p>
       </div>
