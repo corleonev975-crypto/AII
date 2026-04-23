0d03ee26d4f8dbab31566a94a9c86df0ff1f22dd
corleonev975-crypto <corleonev975@gmail.com>
2026-04-23 17:56:57 +0700

    Update HeroAvatar component styles and structure

--- components/hero-avatar.tsx
+++ components/hero-avatar.tsx
@@ -3,25 +3,17 @@
 export default function HeroAvatar() {
   return (
     <div className="relative flex items-center justify-center">
-      
-      {/* glow */}
-      <div className="absolute h-[240px] w-[240px] rounded-full bg-purple-600/20 blur-3xl" />
-
-      {/* ring */}
+      <div className="absolute h-[250px] w-[250px] rounded-full bg-purple-600/20 blur-3xl" />
       <div className="absolute h-[230px] w-[230px] animate-[spin_12s_linear_infinite] rounded-full border border-purple-500/30 border-t-purple-300/70" />
-
-      {/* outer */}
       <div className="absolute h-[200px] w-[200px] rounded-full border border-white/10" />
 
-      {/* avatar */}
-      <div className="relative h-[170px] w-[170px] overflow-hidden rounded-full border border-purple-400/50 bg-black">
+      <div className="relative h-[170px] w-[170px] overflow-hidden rounded-full border border-purple-400/50 bg-black shadow-[0_0_60px_rgba(168,85,247,0.35)]">
         <img
           src="/avatar.gif"
           alt="avatar"
           className="h-full w-full object-cover"
         />
       </div>
-
     </div>
   );
 }
