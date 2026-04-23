72795250c4b21158020808771c3d70c77220670f
corleonev975-crypto <corleonev975@gmail.com>
2026-04-23 19:15:17 +0700

    Update hero-avatar.tsx

--- components/hero-avatar.tsx
+++ components/hero-avatar.tsx
@@ -4,10 +4,11 @@ export default function HeroAvatar() {
   return (
     <div className="relative flex items-center justify-center">
       <div className="absolute h-[260px] w-[260px] rounded-full bg-purple-600/20 blur-3xl" />
-      <div className="absolute h-[230px] w-[230px] animate-[spin_12s_linear_infinite] rounded-full border border-purple-500/35 border-t-purple-300/80" />
-      <div className="absolute h-[200px] w-[200px] rounded-full border border-white/10" />
+      <div className="absolute h-[230px] w-[230px] rounded-full border border-purple-500/40" />
+      <div className="absolute h-[205px] w-[205px] rounded-full border border-purple-500/50" />
+      <div className="absolute h-[185px] w-[185px] rounded-full border border-purple-400/30" />
 
-      <div className="relative h-[170px] w-[170px] overflow-hidden rounded-full border border-purple-400/50 bg-black shadow-[0_0_60px_rgba(168,85,247,0.35)]">
+      <div className="relative h-[170px] w-[170px] overflow-hidden rounded-full border border-purple-400/50 bg-black shadow-[0_0_60px_rgba(168,85,247,0.4)]">
         <img
           src="/avatar.gif"
           alt="avatar"
